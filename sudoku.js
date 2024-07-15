7b0dd611fdf1a0d2b6e45024cf3d5ccea69bc349
RaquibTaj <84203410+RaquibTaj@users.noreply.github.com>
2024-07-15 17:15:38 -0400

    Added: Board

--- sudoku.js
+++ sudoku.js
@@ -42,4 +42,13 @@ function setGame() {
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
+
+    for (let r=0; r<9;r++){
+        for (let c=0; c<9; c++){
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
 }
